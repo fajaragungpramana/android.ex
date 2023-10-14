120479d5bea10541f1d38ea169616900e3542c5a
fajaragungpramana <fajar.agungpramana01@gmail.com>
2023-10-14 21:14:42 +0700

    Add typography style.

--- widget/src/main/java/com/github/fajaragungpramana/ex/widget/style/Type.kt
+++ widget/src/main/java/com/github/fajaragungpramana/ex/widget/style/Type.kt
@@ -2,33 +2,28 @@ package com.github.fajaragungpramana.ex.widget.style
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.github.fajaragungpramana.ex.widget.R
 
-// Set of Material typography styles to start with
+val PoppinsFamily = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_semi_bold, FontWeight.SemiBold)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    headlineLarge = TextStyle(
+        fontFamily = PoppinsFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 40.sp,
+        color = Black100
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    bodyMedium = TextStyle(
+        fontFamily = PoppinsFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        color = Black50
     )
-    */
 )
