21743ad185137058382d44e002dff233e472d9f1
fajaragungpramana <fajar.agungpramana01@gmail.com>
2023-10-15 13:20:57 +0700

    Add back button.

--- widget/src/main/java/com/github/fajaragungpramana/ex/widget/component/LargeTopAppBarComponent.kt
+++ widget/src/main/java/com/github/fajaragungpramana/ex/widget/component/LargeTopAppBarComponent.kt
@@ -1,7 +1,11 @@
 package com.github.fajaragungpramana.ex.widget.component
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,7 +15,11 @@ object LargeTopAppBarComponent {
 
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun WithSubtitle(title: String, subtitle: String) {
+    inline fun WithSubtitle(
+        title: String,
+        subtitle: String,
+        crossinline onClickBackListener: (Boolean) -> Boolean = { true }
+    ) {
 
         LargeTopAppBar(
             title = {
@@ -27,6 +35,15 @@ object LargeTopAppBarComponent {
                         style = MaterialTheme.typography.bodyMedium
                     )
 
+                }
+            },
+            navigationIcon = {
+                if (onClickBackListener(false)) {
+                    IconButton(onClick = {
+                        onClickBackListener(true)
+                    }) {
+                        Icon(Icons.Filled.ArrowBack, null)
+                    }
                 }
             }
         )
